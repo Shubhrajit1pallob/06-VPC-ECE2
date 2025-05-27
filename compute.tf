3ad7c68fcfbe34bdffe1b14ecdd6148265c3bcdd
Shubhrajit1pallob <spallob@asu.edu>
2025-05-27 13:59:30 -0700

    Created custom security Groups for our EC2 instance. They are both ingress rules.

--- compute.tf
+++ compute.tf
@@ -3,9 +3,40 @@ resource "aws_instance" "web" {
   associate_public_ip_address = true
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.public_http_traffic.id]
   root_block_device {
     delete_on_termination = true
     volume_size           = 8
     volume_type           = "gp2"
   }
+
+  tags = merge(local.common_tags, {
+    Name = "06-resources-ec2"
+  })
+}
+
+resource "aws_security_group" "public_http_traffic" {
+  description = "Rule to allow HTTP traffic from the internet on port 443 and 80"
+  name        = "public_http_traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(local.common_tags, {
+    Name = "06-resources-sg"
+  })
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
 }
