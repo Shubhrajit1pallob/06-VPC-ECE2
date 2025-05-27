165b9d2d5a062213873017058e09cef684e5a333
Shubhrajit1pallob <spallob@asu.edu>
2025-05-27 12:59:31 -0700

    Created resources for a internet gateway and connected them through an association.

--- networking.tf
+++ networking.tf
@@ -11,8 +11,8 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
 
   tags = {
     ManagedBy   = "Terraform"
@@ -23,3 +23,37 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    ManagedBy   = "Terraform"
+    Name        = "06-internet-gateway"
+    Project     = "06-Resources"
+    Environment = "Development"
+    Owner       = "Shubhrajit Pallob"
+  }
+}
+
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    ManagedBy   = "Terraform"
+    Name        = "06-route-table"
+    Project     = "06-Resources"
+    Environment = "Development"
+    Owner       = "Shubhrajit Pallob"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.main.id
+}
